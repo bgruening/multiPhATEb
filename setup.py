3433a09f40022ec0e076091056828f10dc39b02f
carolzhou <carol.zhou@comcast.net>
2019-02-23 17:52:37 -0800

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='multiPhate',
+	version='0.5',
+	description='Phage annotation pipeline',
+	author='Carol Zhou'
+	author_email='zhou4@llnl.gov',
+	url='https://github.com/carolzhou/multiPhATE',
+	packages=[]'
+	)
