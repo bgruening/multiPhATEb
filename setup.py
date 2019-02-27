b80f475962c55604046e65ea6df416ed57d7b12a
carolzhou <carol.zhou@comcast.net>
2019-02-27 15:30:14 -0800

    Correcting syntax err in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(name='multiPhate',
 	version='0.5',
 	description='Phage annotation pipeline',
-	author='Carol Zhou'
+	author='Carol Zhou',
 	author_email='zhou4@llnl.gov',
 	url='https://github.com/carolzhou/multiPhATE',
 	packages=[]'
